06fd2580669351b1b38d9a74b8dcd97c21f0463e
devm567 <dev.sot010004@pwioi.com>
2025-09-14 00:53:31 +0530

    added-urls-data

---
+++ src/assests/Urls.jsx
@@ -0,0 +1,13 @@
+const Urls = {
+    "gaming" : "https://raw.githubusercontent.com/endsem2025/milkydrip-assests/refs/heads/main/gaming/gaming_products.json",
+    "horror" : "https://raw.githubusercontent.com/endsem2025/milkydrip-assests/refs/heads/main/horror/horror_products.json",
+    "movies" : "https://raw.githubusercontent.com/endsem2025/milkydrip-assests/refs/heads/main/movies/movies_products.json",
+    "tv-shows" : "https://raw.githubusercontent.com/endsem2025/milkydrip-assests/refs/heads/main/tv-shows/tv-shows_products.json",
+    "zodiac" : "https://raw.githubusercontent.com/endsem2025/milkydrip-assests/refs/heads/main/zodiac/gemini_products.json",
+    "nostalgia" : "https://raw.githubusercontent.com/endsem2025/milkydrip-assests/refs/heads/main/nostalgia/nostalgic_products.json",
+    "top-picks" : "https://raw.githubusercontent.com/endsem2025/milkydrip-assests/refs/heads/main/top-picks/top-picks_products.json",
+    "new-releases" : "https://raw.githubusercontent.com/endsem2025/milkydrip-assests/refs/heads/main/new-releases/weeklies_products.json",
+    "other" : "https://raw.githubusercontent.com/endsem2025/milkydrip-assests/refs/heads/main/other-collections/funny_products.json"
+};
+
+export default Urls;
